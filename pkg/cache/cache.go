3dd7fa1c43708c301e284b31f090be5bbc11844a
TOomaAh <tbrunet72@gmail.com>
2026-06-09 23:34:16 +0200

    feat: initialize memory cache

---
+++ pkg/cache/cache.go
@@ -0,0 +1,32 @@
+package cache
+
+import (
+	"fmt"
+	"time"
+)
+
+type Cache interface {
+	Set(key string, value interface{}, duration time.Duration)
+	Get(key string) (interface{}, bool)
+	Delete(key string)
+}
+
+type CacheConfig struct {
+	DefaultExpiration time.Duration
+	CleanupInterval   time.Duration
+}
+
+type CACHE_TYPE string
+
+const (
+	MEMORY_CACHE_TYPE CACHE_TYPE = "MEMORY"
+)
+
+func NewCache(cacheType CACHE_TYPE, config *CacheConfig) (Cache, error) {
+	switch cacheType {
+	case MEMORY_CACHE_TYPE:
+		return NewMemoryCache(config.DefaultExpiration, config.CleanupInterval), nil
+	default:
+		return nil, fmt.Errorf("cannot initialize %s, cache not implemented", cacheType)
+	}
+}
