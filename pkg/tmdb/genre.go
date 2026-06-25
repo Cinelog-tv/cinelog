3f242b064b1cf3b5461dbde951e05d01deb6e6f2
TOomaAh <tbrunet72@gmail.com>
2026-06-25 23:23:34 +0200

    feat: add genre endpoint

--- pkg/tmdb/genre.go
+++ pkg/tmdb/genre.go
@@ -1,5 +1,15 @@
 package tmdb
 
+import (
+	"net/url"
+	"strings"
+)
+
+const (
+	GENRE_MOVIE_ENDPOINT = "/genre/movie/list"
+	GENRE_TV_ENDPOINT    = "/genre/tv/list"
+)
+
 type TMDBGenreResponse struct {
 	Genres []TMDBGenre `json:"genres"`
 }
@@ -7,4 +17,36 @@ type TMDBGenreResponse struct {
 type TMDBGenre struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
+}
+
+type TMDBGenreParameters struct {
+	Language string `json:"language"`
+}
+
+func (client *TMDBClient) GetMovieGenres(parameters *TMDBGenreParameters) (*TMDBGenreResponse, error) {
+	return client.getGenre(parameters, GENRE_MOVIE_ENDPOINT)
+}
+
+func (client *TMDBClient) GetTVGenres(parameters *TMDBGenreParameters) (*TMDBGenreResponse, error) {
+	return client.getGenre(parameters, GENRE_TV_ENDPOINT)
+}
+
+func (client *TMDBClient) getGenre(parameters *TMDBGenreParameters, endpoint string) (*TMDBGenreResponse, error) {
+	queryParams := url.Values{}
+	if v := strings.TrimSpace(parameters.Language); v != "" {
+		queryParams.Add("language", v)
+	}
+
+	req, err := client.newRequest("GET", endpoint, &queryParams, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var tmdbGenreResponse TMDBGenreResponse
+	if err := client.do(req, &tmdbGenreResponse); err != nil {
+		return nil, err
+	}
+
+	return &tmdbGenreResponse, nil
 }
